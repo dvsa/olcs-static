b45ebeee2344f9ba3a0f4a19d6d9ce14783bb498
Sam Quayle <samquayle@Sams-MacBook-Pro.local>
2014-03-11 17:21:59 +0000

    Created the tables page and began styling

---
+++ templates/tables.php
@@ -0,0 +1,52 @@
+<?php
+	$pageTitle = "OLCS Tables";
+	$pageSubtitle = "Everything you need to create complex tables";
+?>
+		<?php include 'includes/global-header.php'; ?>
+
+		<div class="page-wrapper">
+			<?php include 'includes/global-breadcrumbs.php'; ?>
+
+			<div id="main" role="main" tabindex="-1" class="main">
+
+				<?php include 'includes/page-header.php';  ?>
+				<table>
+					<thead>
+						<tr>
+							<th>Licence number</th>
+							<th>Operator / trading name</th>
+							<th>Postcode</th>
+							<th>First name</th>
+							<th>Last name</th>
+							<th>Date of birth</th>
+							<th>Convictions</th>
+						</tr>
+					</thead>
+					<tbody>
+					<tr>
+						<td><a href="">76163222712</a></td>
+						<td><a href="">Jim Jefferey's Haulage LTD</a></td>
+						<td>LS7 4QP</td>
+						<td>James</td>
+						<td>Jefferey</td>
+						<td>07/02/1963</td>
+						<td><a href="">14</a></td>
+					</tr>
+					<tr>
+						<td><a href="">362332422712</a></td>
+						<td><a href="">Amber Taxis</a></td>
+						<td>LS3 2PS</td>
+						<td>Mohammed</td>
+						<td>Ali</td>
+						<td>23/11/1943</td>
+						<td><a href="">5</a></td>
+					</tr>
+					</tbody>
+				</table>
+
+			</div><!-- end main -->
+
+		</div><!-- end page-wrapper -->
+
+<?php include 'includes/global-footer.php'; ?>
+		
