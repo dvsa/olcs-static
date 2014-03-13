db380a6cd6de9033818a9a385afd2f10e7c143ee
sam.quayle@valtech.co.uk <samquayle@sams-mbp.home>
2014-03-13 14:10:26 +0000

    Added a header and actions to the tables, added crest to the footer

--- templates/includes/global-footer.php
+++ templates/includes/global-footer.php
@@ -3,8 +3,11 @@
 				<ul class="global-footer__links">
 					<li class="global-footer__link"><a href="">Helpdesk</a></li>
 					<li class="global-footer__link"><a href="">Privacy &amp; Cookies</a></li>
-					<li class="global-footer__link">&copy; OLCS. All rights Reserved</li>
+					<li class="global-footer__link">Built by <a href="">Driving Standards Agency</a></li>
 				</ul>
+				<div class="global-footer__copyright">
+					<a href="">&copy; Crown Copyright</a>
+				</div>
 		    </div>
 		</div><!-- end global-footer -->
 	</body>
--- templates/tables.php
+++ templates/tables.php
@@ -10,10 +10,19 @@
 			<div id="main" role="main" tabindex="-1" class="main">
 
 				<?php include 'includes/page-header.php';  ?>
+				<div class="table__header">				
+					<h2>Table header</h2>
+					<div class="actions--right">
+						<input class="button--primary" type="submit" value="Add">
+						<input class="button--secondary" type="submit" value="Edit" disabled="disabled">
+						<input class="button--warning" type="submit" value="Delete" disabled="disabled">
+					</div><!-- end actions- right -->
+				</div><!-- end table__header -->
 				<div class="table__wrapper">
 					<table>
 						<thead>
 							<tr>
+								<th><input type="checkbox"></th>
 								<th>Licence number</th>
 								<th>Operator / trading name</th>
 								<th>Postcode</th>
@@ -25,6 +34,7 @@
 						</thead>
 						<tbody>
 						<tr>
+							<td><input type="checkbox"></td>
 							<td><a href="">76163222712</a></td>
 							<td><a href="">Jim Jefferey's Haulage LTD</a></td>
 							<td>LS7 4QP</td>
@@ -34,6 +44,27 @@
 							<td><a href="">14</a></td>
 						</tr>
 						<tr>
+							<td><input type="checkbox"></td>
+							<td><a href="">3623324227</a></td>
+							<td><a href="">Amber Taxis</a></td>
+							<td>LS3 2PS</td>
+							<td>Mohammed</td>
+							<td>Ali</td>
+							<td>23/11/1943</td>
+							<td><a href="">5</a></td>
+						</tr>
+						<tr>
+							<td><input type="checkbox"></td>
+							<td><a href="">76163222712</a></td>
+							<td><a href="">Jim Jefferey's Haulage LTD</a></td>
+							<td>LS7 4QP</td>
+							<td>James</td>
+							<td>Jefferey</td>
+							<td>07/02/1963</td>
+							<td><a href="">14</a></td>
+						</tr>
+						<tr>
+							<td><input type="checkbox"></td>
 							<td><a href="">3623324227</a></td>
 							<td><a href="">Amber Taxis</a></td>
 							<td>LS3 2PS</td>
@@ -44,10 +75,7 @@
 						</tr>
 						</tbody>
 					</table>
-				</div>
-				<div class="actions">
-					<input class="button--primary" type="submit" value="Add another row">
-				</div><!-- end actions -->
+				</div><!-- end table__wrapper -->
 
 			</div><!-- end main -->
 
