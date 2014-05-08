b7e6dd5bb744630a1de98e0f83561f886d4c067a
Sam Quayle <sam.quayle@valtech.co.uk>
2014-05-08 11:44:22 +0100

    Updated some field styling on forms

--- public/js/main.js
+++ public/js/main.js
@@ -1,89 +1,10 @@
-var OLCS = OLCS || {};
+require.config({
+    baseUrl: 'js/lib',
+    paths: {
+        jquery: 'jquery.1.11.0'
+    }
+});
 
-/*
- * Progressively reveals elements based on form element change actions
- */
-
-OLCS.hiddenFields = (function () {
-		
-	var scriptTag = $('#inline-js');
-
-	function parseJSON () {	
-		return json = JSON.parse(scriptTag.html()).hiddenFieldsList;
-	}
-
-	function hideFields () {
-		$.each(json, function() {
-			$('' + this.hiddenElement + '').hide();
-		});
-	}
-
-	function toggleFields () {
-		$.each(json, function(e) {
-			var $fieldName = $('[name="' + this.fieldName + '"]'),
-			$hiddenElement = $('' + this.hiddenElement + ''),
-			fieldValue = this.fieldValue;
-
-			$fieldName.change(function() {
-				if ( $(this).val() === fieldValue ) {
-					$hiddenElement.show();
-				} else {
-				}
-			});
-		});
-	}
-
-	function init () {
-		if (!scriptTag.length) {
-			return false;
-		} else {
-			parseJSON();
-			hideFields();
-			toggleFields();
-		}	
-	}
-
-    return {
-        init: init,
-        scriptTag: scriptTag,
-        parseJSON: parseJSON,
-        hideFields: hideFields,
-        toggleFields: toggleFields
-    };
-
-})()
-
-
-
-/*
- * Removes string from form labels"
- */
-
-// OLCS.removeString = (function () {
-
-
-// 	function checkForString () {
-// 		$('label, legend').each(function() {
-// 		  	var string = $(this).text();
-// 		    string = string.replace(/\(if applicable\)/, '');
-// 		    $(this).text(string);
-// 		});
-// 	}
-
-// 	function init () {
-// 		checkForString();
-// 	}
-
-//     return {
-//     	init: init,
-//     	checkForString: checkForString
-//     };
-
-// })()
-
-
-
-$(function(){
-	OLCS.hiddenFields.init();
-	// removeString.init();
+require(['jquery'], function( $ ) {
+    console.log( $ );
 });
--- public/js/styleguide.js
+++ public/js/styleguide.js
@@ -1,213 +1,212 @@
+define([], function () { /*some email validator function*/ });
 
 
+// $( document ).ready(function() {
 
+// 		$('.row').css({'width':'100%','overflow':'auto', 'margin-bottom': '2.5em'});
+// 		var docUrl = document.location.href;
 
-$( document ).ready(function() {
-
-		$('.row').css({'width':'100%','overflow':'auto', 'margin-bottom': '2.5em'});
-		var docUrl = document.location.href;
-
-		if (docUrl.indexOf('form-elements') > -1) {
-			toggleValidation();
-		}
-		else if (docUrl.indexOf('alert') > -1) {
-			toggleOverlay();
-		}
-		else if (docUrl.indexOf('layouts') > -1) {
-			layoutStyles();
-		}
-		else if (docUrl.indexOf('popup-table') > -1) {
-			ajaxModal('/styleguide/popup-table-no-js','.modal','#main');
-			toggleOverlay();
-		}
-		else if (docUrl.indexOf('popup-form') > -1) {
-			ajaxModal('/styleguide/popup-form-no-js','.modal','#main');
-			toggleOverlay();
-		}
-		else if (docUrl.indexOf('operating-centres') > -1) {
-			modal('.modal','#main');
-		}
-		else if (docUrl.indexOf('compliance') > -1) {
-			showHide('.case-summary__toggle','.case-summary__list');
-			$('body').addClass('internal');
-		}
-		else if (docUrl.indexOf('form-js') > -1) {
-			validation();
-		}
-});
-
-
-
-
-
-
-	/*
-	 * Toggle validation styling on and off
-	 */
-
-	function toggleValidation() {
-		$('body').prepend('<style>.toggler { position: fixed; display: block; background: black; bottom: 0; right: 0; color: white; padding: 0.25em 0.5em; } .toggler a {color: white;} </style><div class="toggler"><a href="#">Validation</a></div>');
-		$('.toggler a').click(function(e) {
-			e.preventDefault();
-			$('.validation-wrapper, .validation-summary').toggle();
-		});
-	}
-
-
-	/*
-	 * Toggle overlay visibility
-	 */
-
-	function toggleOverlay() {
-		$('input, .button--primary--large, .modal__close').click(function(e) {
-			e.preventDefault();
-			$('.overlay, .modal__wrapper, .alert__wrapper').toggle();
-		});
-	}
-
-
-	/*
-	 * Styling for layout page in the styleguide
-	 */
-
-	function layoutStyles() {
-		$('.full-width, .one-quarter, .one-third, .two-thirds, .three-quarters, .one-half').css({'background':'#d5d5d5','height':'200px', 'margin-bottom':'5%'});
-	}
+// 		if (docUrl.indexOf('form-elements') > -1) {
+// 			toggleValidation();
+// 		}
+// 		else if (docUrl.indexOf('alert') > -1) {
+// 			toggleOverlay();
+// 		}
+// 		else if (docUrl.indexOf('layouts') > -1) {
+// 			layoutStyles();
+// 		}
+// 		else if (docUrl.indexOf('popup-table') > -1) {
+// 			ajaxModal('/styleguide/popup-table-no-js','.modal','#main');
+// 			toggleOverlay();
+// 		}
+// 		else if (docUrl.indexOf('popup-form') > -1) {
+// 			ajaxModal('/styleguide/popup-form-no-js','.modal','#main');
+// 			toggleOverlay();
+// 		}
+// 		else if (docUrl.indexOf('operating-centres') > -1) {
+// 			modal('.modal','#main');
+// 		}
+// 		else if (docUrl.indexOf('compliance') > -1) {
+// 			showHide('.case-summary__toggle','.case-summary__list');
+// 			$('body').addClass('internal');
+// 		}
+// 		else if (docUrl.indexOf('form-js') > -1) {
+// 			validation();
+// 		}
+// });
 
 
 
 
 
-	function ajaxModal(url,$targetEl,$fragmentEl) {
-		$.ajax({
-			url: url,
-			type: 'GET',
-			success: function(data) {
-				$($targetEl).append($(data).find($fragmentEl));
-			}
-		});
-	}
+
+// 	/*
+// 	 * Toggle validation styling on and off
+// 	 */
+
+// 	function toggleValidation() {
+// 		$('body').prepend('<style>.toggler { position: fixed; display: block; background: black; bottom: 0; right: 0; color: white; padding: 0.25em 0.5em; } .toggler a {color: white;} </style><div class="toggler"><a href="#">Validation</a></div>');
+// 		$('.toggler a').click(function(e) {
+// 			e.preventDefault();
+// 			$('.validation-wrapper, .validation-summary').toggle();
+// 		});
+// 	}
 
 
-	/*
-	 * Generic Ajax overlay function
-	 */
+// 	/*
+// 	 * Toggle overlay visibility
+// 	 */
 
-	function modal($targetEl,$fragmentEl) {
-		// Hide the overlay on load
-		$('.overlay, .modal__wrapper').hide();
-
-		$('.js-modal').click(function(e){
-			e.preventDefault();
-
-			// Ajax request
-			$.ajax({
-				url: $(this).attr('href'),
-				type: 'GET',
-				success: function(data) {
-					$($targetEl).append($(data).find($fragmentEl));
-					$('.overlay, .modal__wrapper').show();
-				}
-			});
-		});
-
-		// Close the modal
-		$('.modal__close').click(function(){
-			$('.overlay, .modal__wrapper').hide();
-		});
-	}
+// 	function toggleOverlay() {
+// 		$('input, .button--primary--large, .modal__close').click(function(e) {
+// 			e.preventDefault();
+// 			$('.overlay, .modal__wrapper, .alert__wrapper').toggle();
+// 		});
+// 	}
 
 
-	/*
-	 * Progressively discloses page element
-	 */
+// 	/*
+// 	 * Styling for layout page in the styleguide
+// 	 */
 
-	// (function($) {
-	// 	 $.fn.progressiveDisclosure = function(options) {
-
-	// 		var opts = $.extend({}, $.fn.progressiveDisclosure.defaults, options);
-
-	// 		$(trigger).click(function() {
-	// 			$.ajax({
-	// 				url: url,
-	// 				type: 'GET',
-	// 				success: function(data) {
-	// 					$(data).find(fragment).insertAfter(target);
-	// 				}
-	// 			});
-	// 		});
-
-	// 	    $.fn.progressiveDisclosure.defaults = {
-	// 			trigger : '.radio-female',
-	// 			url : 'progressive-form-2',
-	// 	        fragment : '#another-fieldset',
-	// 	        target : '#personal-information fieldset'
-	// 	    };
-
-	// 	 };
-	// })(jQuery);
-
-		// if (jsonArray.length > 0) {
-		// }
-
-		// $.each(jsonArray, function() {
-		// 	var jsonName = this.name,
-		// 		jsonValue = this.value;
-
-		// 	$('[name="' + this.name + '"]').change(function() {
-		// 		if ( $(this).val() === jsonValue ) {
-		// 			alert('hello');
-		// 		}
-		// 	});
-		// });
+// 	function layoutStyles() {
+// 		$('.full-width, .one-quarter, .one-third, .two-thirds, .three-quarters, .one-half').css({'background':'#d5d5d5','height':'200px', 'margin-bottom':'5%'});
+// 	}
 
 
 
 
-	/*
-	 * Generic show & hide 
-	 */
 
-	function showHide($target,$element) {
-		$($target).click(function(e) {
-			e.preventDefault();
-			$($element).toggle();
-		});
-	}
+// 	function ajaxModal(url,$targetEl,$fragmentEl) {
+// 		$.ajax({
+// 			url: url,
+// 			type: 'GET',
+// 			success: function(data) {
+// 				$($targetEl).append($(data).find($fragmentEl));
+// 			}
+// 		});
+// 	}
 
 
-	/*
-	 * Validation
-	 */
+// 	/*
+// 	 * Generic Ajax overlay function
+// 	 */
 
-	function validation() {
+// 	function modal($targetEl,$fragmentEl) {
+// 		// Hide the overlay on load
+// 		$('.overlay, .modal__wrapper').hide();
 
-		$('#form--sign-up').validate({
-			debug: true,
-			errorLabelContainer: 'validation-wrapper',
-			wrapper: 'ul class="validation-wrapper__message"',
-			errorElement:'li',
-			errorPlacement: function(error, element) {
-				error.insertBefore(element.parent('.field'));
-				$('.validation-wrapper__message').next('.field').andSelf().wrapAll('<div class="validation-wrapper"/>');
-			},
-			rules: {
-				'address[line-1]': {
-					required: true,
-					minlength: 2,
-					maxlength: 5
-				},
-				'gender': {
-					required: true
-				}
-			},
-			messages: {
-				'address[line-1]': {
-					required: 'Please enter the first line in your address',
-				}
-			}
-		});
-	}
+// 		$('.js-modal').click(function(e){
+// 			e.preventDefault();
+
+// 			// Ajax request
+// 			$.ajax({
+// 				url: $(this).attr('href'),
+// 				type: 'GET',
+// 				success: function(data) {
+// 					$($targetEl).append($(data).find($fragmentEl));
+// 					$('.overlay, .modal__wrapper').show();
+// 				}
+// 			});
+// 		});
+
+// 		// Close the modal
+// 		$('.modal__close').click(function(){
+// 			$('.overlay, .modal__wrapper').hide();
+// 		});
+// 	}
+
+
+// 	/*
+// 	 * Progressively discloses page element
+// 	 */
+
+// 	// (function($) {
+// 	// 	 $.fn.progressiveDisclosure = function(options) {
+
+// 	// 		var opts = $.extend({}, $.fn.progressiveDisclosure.defaults, options);
+
+// 	// 		$(trigger).click(function() {
+// 	// 			$.ajax({
+// 	// 				url: url,
+// 	// 				type: 'GET',
+// 	// 				success: function(data) {
+// 	// 					$(data).find(fragment).insertAfter(target);
+// 	// 				}
+// 	// 			});
+// 	// 		});
+
+// 	// 	    $.fn.progressiveDisclosure.defaults = {
+// 	// 			trigger : '.radio-female',
+// 	// 			url : 'progressive-form-2',
+// 	// 	        fragment : '#another-fieldset',
+// 	// 	        target : '#personal-information fieldset'
+// 	// 	    };
+
+// 	// 	 };
+// 	// })(jQuery);
+
+// 		// if (jsonArray.length > 0) {
+// 		// }
+
+// 		// $.each(jsonArray, function() {
+// 		// 	var jsonName = this.name,
+// 		// 		jsonValue = this.value;
+
+// 		// 	$('[name="' + this.name + '"]').change(function() {
+// 		// 		if ( $(this).val() === jsonValue ) {
+// 		// 			alert('hello');
+// 		// 		}
+// 		// 	});
+// 		// });
+
+
+
+
+// 	/*
+// 	 * Generic show & hide 
+// 	 */
+
+// 	function showHide($target,$element) {
+// 		$($target).click(function(e) {
+// 			e.preventDefault();
+// 			$($element).toggle();
+// 		});
+// 	}
+
+
+// 	/*
+// 	 * Validation
+// 	 */
+
+// 	function validation() {
+
+// 		$('#form--sign-up').validate({
+// 			debug: true,
+// 			errorLabelContainer: 'validation-wrapper',
+// 			wrapper: 'ul class="validation-wrapper__message"',
+// 			errorElement:'li',
+// 			errorPlacement: function(error, element) {
+// 				error.insertBefore(element.parent('.field'));
+// 				$('.validation-wrapper__message').next('.field').andSelf().wrapAll('<div class="validation-wrapper"/>');
+// 			},
+// 			rules: {
+// 				'address[line-1]': {
+// 					required: true,
+// 					minlength: 2,
+// 					maxlength: 5
+// 				},
+// 				'gender': {
+// 					required: true
+// 				}
+// 			},
+// 			messages: {
+// 				'address[line-1]': {
+// 					required: 'Please enter the first line in your address',
+// 				}
+// 			}
+// 		});
+// 	}
 
 
 
