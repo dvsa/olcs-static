3bb4cdd59c402b3060ee568588c8d279c128dc62
Sam Quayle <sam.quayle@valtech.co.uk>
2014-05-12 10:55:54 +0100

    Added in number classes to application navigation

--- public/js/main.js
+++ public/js/main.js
@@ -6,19 +6,6 @@ require.config({
 
 require(['module/hiddenFields'], function(hiddenFields) {
 
-	hiddenFieldsList = [
-	    {
-	        "fieldName": "gender",
-	        "fieldValue": "female",
-	        "hiddenElement": "#favourite-colour-field"
-	    },
-	    {
-	        "fieldName": "favourite-colour",
-	        "fieldValue": "green",
-	        "hiddenElement": "#favourite-month-field"
-	    }
-	];
-
 	hiddenFields.init();
 
 });
--- public/js/module/hiddenFields.js
+++ public/js/module/hiddenFields.js
@@ -1,24 +1,42 @@
 define(["jquery"], function() {
 	return {
 
+		parseJSON : function() {
+			var scriptTag = $('#inline-js');
+			return hiddenFieldsList = JSON.parse(scriptTag.html()).hiddenFieldsList;
+		},
+
 		hideFields : function() {
+			this.parseJSON();
 			$.each(hiddenFieldsList, function() {
 				$('' + this.hiddenElement + '').hide();
+				$('' + this.hiddenElement + ' input,' + this.hiddenElement + ' select').prop('disabled', true);
 			});
 		},
 
 		toggleFields : function() {
 			$.each(hiddenFieldsList, function(e) {
-				var $fieldName = $('[name="' + this.fieldName + '"]'),
-				$hiddenElement = $('' + this.hiddenElement + ''),
-				fieldValue = this.fieldValue;
+				var $fieldName 
+				= $('[name="' + this.fieldName + '"]'),
+					$hiddenElement = $('' + this.hiddenElement + ''),
+					fieldValue = this.fieldValue,
+					listLength = hiddenFieldsList.length;
+
 				$fieldName.change(function() {
 					if ( $(this).val() === fieldValue ) {
 						$hiddenElement.show();
+						$hiddenElement.find('select, input').removeAttr('disabled');
 					} else {
-						$hiddenElement.hide();
+						for (var i = e; i < listLength; i++) {
+							$('' + hiddenFieldsList[i].hiddenElement + '').hide();
+							$('' + hiddenFieldsList[i].hiddenElement + ' input').prop('checked', false)
+								.prop('disabled', true);
+							$('' + hiddenFieldsList[i].hiddenElement + ' select').val('')
+								.prop('disabled', true);
+						}
 					}
 				});
+
 			});
 		},
 
