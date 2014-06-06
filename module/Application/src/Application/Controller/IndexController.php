bc4dde4bc19712fc80ec5f83c826c6345f3119c5
Rob Caiger <rob@clocal.co.uk>
2014-06-06 11:55:28 +0100

    CS fix

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -1,17 +1,20 @@
 <?php
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
 
+/**
+ * IndexController
+ *
+ * @author Someone <someone@valtech.co.uk>
+ */
 namespace Application\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
+/**
+ * IndexController
+ *
+ * @author Someone <someone@valtech.co.uk>
+ */
 class IndexController extends AbstractActionController
 {
     public function notFoundAction()
