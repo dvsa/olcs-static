1f5741574e4b094d05964a0025c0527ac26b6879
Nick Payne <nick@kurai.co.uk>
2014-07-31 14:43:01 +0100

    add start of very simple form helper

---
+++ assets/_js/common/formHelper.js
@@ -0,0 +1,35 @@
+var OLCS = OLCS || {};
+
+OLCS.formHelper = (function(document, $, undefined) {
+
+  'use strict';
+
+  /**
+   * Expose a jQuery-esque function which tries to work
+   * out which actual public property to invoke purely
+   * based off argument length. Pretty crude, but a
+   * handy shorthand
+   */
+  var exports = function() {
+    switch (arguments.length) {
+      case 1:
+        return exports.fieldset.apply(null, arguments);
+      case 2:
+        return exports.input.apply(null, arguments);
+    }
+  };
+
+  /**
+   * public interface
+   */
+  exports.fieldset = function(selector) {
+    return $("fieldset[data-group='" + selector + "']");
+  };
+
+  exports.input = function(fieldset, name) {
+    return $("[name=" + fieldset + "\\[" + name + "\\]]");
+  };
+
+  return exports;
+
+}(document, window.jQuery));
