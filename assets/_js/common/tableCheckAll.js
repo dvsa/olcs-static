69081ad12a7659f75f7450ccd341af444f8792a9
Rob Caiger <rob@clocal.co.uk>
2014-09-17 15:38:30 +0100

    Added the table checkall component, and the unit tests

---
+++ assets/_js/common/tableCheckAll.js
@@ -0,0 +1,20 @@
+
+OLCS.tableCheckAll = (function(document, $, undefined) {
+
+  return function init(options) {
+
+    var defaultOptions = {
+      'selector': 'input[name="checkall"]'
+    };
+
+    options = $.extend(defaultOptions, options);
+
+    $(document).on('change', options.selector, function() {
+
+      var table = $(this).closest('table');
+
+      $(table).find('input[type="checkbox"]').not(options.selector).prop('checked', $(this).is(':checked'));
+    });
+  };
+
+}(document, window.jQuery));
