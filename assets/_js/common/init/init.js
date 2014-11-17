422ff071490a3c2a6a8856c626c4e2aa66761186
Nick Payne <nick@kurai.co.uk>
2014-11-17 16:00:42 +0000

    start of preloader logic

--- assets/_js/common/init/init.js
+++ assets/_js/common/init/init.js
@@ -1,12 +1,14 @@
 $(function() {
   "use strict";
 
-  // always append a hidden element to all forms so we know they were submitted
-  // with a JS enabled browser. The backend can then optionally look for this
-  // known value and potentially react accordingly
-  //
-  // we don't really like having initialisation logic in common but this is so
-  // minor anything else would feel a bit overblown. Can refactor if needed.
+  /*
+   * always append a hidden element to all forms so we know they were submitted
+   * with a JS enabled browser. The backend can then optionally look for this
+   * known value and potentially react accordingly
+   *
+   * we don't really like having initialisation logic in common but this is so
+   * minor anything else would feel a bit overblown. Can refactor if needed.
+   */
   $("form").append("<input type='hidden' name='js-submit' value='1' />");
 
   // allow any links to opt-in to ajax modals
@@ -21,4 +23,14 @@ $(function() {
   $(".chosen-select-medium").chosen({width: "53%"});
   $(".chosen-select-large").chosen({width: "97%"});
 
+  /*
+   * By default, opt-in to showing a preloader for
+   * all AJAX requests. If this proves to be a bit greedy
+   * we can make it a bit more selective :)
+   */
+  $(document).bind("ajaxSend", function() {
+    OLCS.preloader.show();
+  }).bind("ajaxComplete", function() {
+    OLCS.preloader.hide();
+  });
 });
---
+++ test/js/preloader.test.js
@@ -0,0 +1,70 @@
+describe("OLCS.preloader", function() {
+  "use strict";
+
+  beforeEach(function() {
+    this.component = OLCS.preloader;
+  });
+
+  it("should be defined", function() {
+    expect(this.component).to.exist;
+  });
+
+  it("should expose the correct public interface", function() {
+    expect(this.component.show).to.be.a("function");
+    expect(this.component.hide).to.be.a("function");
+  });
+
+  describe("show", function() {
+    beforeEach(function() {
+      this.component.show();
+    });
+
+    it("shows the preloader wrapper", function() {
+      expect($(".preloader__wrapper").is(":visible")).to.be(true);
+    });
+
+    it("shows the preloader", function() {
+      expect($(".preloader").is(":visible")).to.be(true);
+    });
+
+    it("shows the preloader icon", function() {
+      expect($(".preloader__icon").is(":visible")).to.be(true);
+    });
+
+    describe("When called again", function() {
+      beforeEach(function() {
+        this.component.show();
+      });
+    });
+
+    it("still shows the preloader wrapper", function() {
+      expect($(".preloader__wrapper").is(":visible")).to.be(true);
+    });
+
+    describe("When calling hide", function() {
+      beforeEach(function() {
+        this.fadeOut = sinon.spy($.fn, "fadeOut");
+
+        this.component.hide();
+      });
+
+      afterEach(function() {
+        this.fadeOut.restore();
+      });
+
+      it("does not hide the preloader", function() {
+        expect(this.fadeOut.callCount).to.eql(0);
+      });
+
+      describe("When calling hide again", function() {
+        beforeEach(function() {
+          this.component.hide();
+        });
+
+        it("hides the preloader", function() {
+          expect(this.fadeOut.callCount).to.eql(1);
+        });
+      });
+    });
+  });
+});
