b03d260c9df2b92221c22e205d132335144e45dc
Nick Payne <nick@kurai.co.uk>
2014-12-16 14:42:55 +0000

    remove selfserve init logic

--- assets/_js/init/selfserve.js
+++ assets/_js/init/selfserve.js
@@ -1,24 +1,3 @@
 OLCS.ready(function() {
   "use strict";
-
-  var F = OLCS.formHelper;
-
-  $(document).on("click", ".table__header button", function(e) {
-    e.preventDefault();
-
-    var form   = $(this).parents("form");
-    var button = $(this);
-
-    F.pressButton(form, button);
-    OLCS.formAjax({
-      form: form,
-      success: OLCS.normaliseResponse(function(data) {
-        if ($("<div>").html(data.body).find(".validation-summary").length) {
-          $("body .js-body").html(data.body);
-        } else {
-          OLCS.formModal(data);
-        }
-      })
-    });
-  });
 });
