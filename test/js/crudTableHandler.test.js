6b808bca475e96b56c8d45020ea9b4783be30837
Rob Caiger <rob@clocal.co.uk>
2015-01-14 16:05:58 +0000

    Fixed JS unit tests

--- test/js/crudTableHandler.test.js
+++ test/js/crudTableHandler.test.js
@@ -72,7 +72,7 @@ describe("OLCS.crudTableHandler", function() {
       });
 
       it("binds a click handler to the correct selectors", function() {
-        var str = ".table__header button, .table__wrapper input[type=submit]";
+        var str = ".table__header button, .table__wrapper input[type=submit], .table__empty button";
         expect(this.on.firstCall.args[0]).to.equal("click");
         expect(this.on.firstCall.args[1]).to.equal(str);
       });
