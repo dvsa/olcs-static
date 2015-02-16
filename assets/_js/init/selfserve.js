c76cf0bdf550f808be57aa154ee404ea55c2a037
Nick Payne <nick@kurai.co.uk>
2015-02-16 09:34:54 +0000

    fix small jshint errors

--- assets/_js/init/selfserve.js
+++ assets/_js/init/selfserve.js
@@ -1,19 +1,19 @@
 OLCS.ready(function() {
   "use strict";
 
-  // Quick and dirty way of setting the logged in users 
-  // details in localStorage 
+  // Quick and dirty way of setting the logged in users
+  // details in localStorage
 
 	var user = {
     name: localStorage.name,
     company: localStorage.company
-	}
+	};
 
   if (user.name && user.company) {
     $(".user-menu__item:first").html(""+user.name+", <b>"+user.company+"</b>");
-    
-    if ($('.page-header__subtitle').text() === "Operator Licensing Ltd.") {
-      $('.page-header__subtitle').text(user.company);
+
+    if ($(".page-header__subtitle").text() === "Operator Licensing Ltd.") {
+      $(".page-header__subtitle").text(user.company);
     }
 
   }
