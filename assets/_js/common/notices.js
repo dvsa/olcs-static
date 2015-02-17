f47cf5a98b5561dcdaf166353ee4d94efd4a2142
Sam Quayle <sam.quayle@valtech.co.uk>
2015-02-17 10:45:15 +0000

    Adds test for notices component

---
+++ assets/_js/common/notices.js
@@ -0,0 +1,42 @@
+var OLCS = OLCS || {};
+
+/**
+ * OLCS.notices
+ *
+ * Allows the user to close flash messages, and fades them out
+ * automatically after a few seconds
+ */
+
+OLCS.notices = (function(document, $, undefined) {
+
+  'use strict';
+
+  var closeLinkSelector       = '.notice__link';
+  var noticeContainerSelector = '.notice-container';
+
+  return function init() {
+
+    function remove(element) {
+      $(element).remove();
+    }
+
+    $(noticeContainerSelector)
+      .delay(6000)
+      .fadeOut(300, function() {
+        remove($(this));
+    });
+    
+    $(document).on('click', closeLinkSelector, function(e) {
+      e.preventDefault();
+      
+      if ( $(this).closest('div').siblings().length) {
+        remove($(this).closest('div'));
+      } else {
+        remove(noticeContainerSelector);
+      }
+
+    });
+
+  };
+
+}(document, window.jQuery));
