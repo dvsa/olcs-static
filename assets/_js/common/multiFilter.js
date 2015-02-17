a69f06ee56652c91ea3eae478e1391c21a51d272
Nick Payne <nick@kurai.co.uk>
2015-02-17 16:00:15 +0000

    placeholder multi filter

---
+++ assets/_js/common/multiFilter.js
@@ -0,0 +1,19 @@
+var OLCS = OLCS || {};
+
+OLCS.multiFilter = (function(document, $, undefined) {
+
+  "use strict";
+
+  return function init(options) {
+
+    // @TODO cache the `to` object's values,
+    // since we have to remove their optgroups
+
+    // set up a listener on options.from change, and
+    // based on its value, create an optgroup with
+    // the values from the cached set
+    // and set the select value to have this single
+    // optgroup
+  };
+
+}(document, window.jQuery));
