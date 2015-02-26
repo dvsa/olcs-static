a59dc3e6be0c9a081bd8c99f3522412c78e8adde
Sam Quayle <sam.quayle@valtech.co.uk>
2015-02-26 13:40:36 +0000

    Hello

--- assets/_js/common/fileUploader.js
+++ assets/_js/common/fileUploader.js
@@ -48,10 +48,7 @@ OLCS.fileUploader = (function(document, $, undefined) {
       }
     }
 
-    function createNewFileAction(element) {
-
-      // Get the number of uploader actions
-      var actionCount = element.length + 1;
+    function createNewFileAction(element,count) {
 
       // Clone the last file input
       var newfileAction = element.last().clone();
@@ -63,7 +60,7 @@ OLCS.fileUploader = (function(document, $, undefined) {
       var regex = new RegExp(/fileUpload\[(.*?)\]/g);
 
       // Replace ID with the new incremented one 
-      newHTML = newHTML.replace(regex, "fileUpload["+actionCount+"]");
+      newHTML = newHTML.replace(regex, "fileUpload["+count+"]");
 
       // Set the HTML of our new element
       newfileAction.html(newHTML);
@@ -83,55 +80,65 @@ OLCS.fileUploader = (function(document, $, undefined) {
 
     // When the file input is updated
     $(document).on('change', fileInput, function() {    
-      var newFiles         = this.files;
-      var thisParent       = $(this).parents(fileUploader);
-      var thisFileAction   = thisParent.find(fileAction);
-      var allFiles         = [];
+      ;
+      var thisFileUploader = $(this).parents(fileUploader);
+      var thisFileAction   = thisFileUploader.find(fileAction);
+      var thisFileList     = thisFileUploader.find(fileList);
       var domFileList      = [];
+      var noOfFileActions  = thisFileAction.length + 1;
 
-      thisParent.data('allFiles', allFiles);
+      // Add the new file uploader
+      $(this).closest(fileActions).append(createNewFileAction(thisFileAction,noOfFileActions));
 
-      if (newFiles.length) {
-        console.log(thisParent.data());
-        
-        // Add the new file uploader
-        $(this).closest(fileActions).append(createNewFileAction(thisFileAction));
+      // Hide the current file uploader
+      $(this).closest(fileAction).hide();
+      
+      // Get every attached file 
+      thisFileUploader.find(fileInput).each( function() {
 
-        // Hide the current file uploader
-        $(this).closest(fileAction).hide();
-        
-        // Get every file from every file inputs within the parent .file-uploader
-        // and push it to our array
-        thisParent.find(fileInput).each( function() {
-          
-          for (var i = 0; i < this.files.length; i++) {
-            var fileName      = this.files[i].name;
-            var fileSize      = this.files[i].size;
-            var formattedSize = formatFileSizeString(fileSize);
+        if (this.files.length) {
+          var fileName      = this.files[0].name;
+          var formattedSize = formatFileSizeString(this.files[0].size);
 
-            // Populate the domFileList array with dom elements
-            domFileList.push('<div class="file-uploader__item"><p>'+ 
-              fileName +
-              ' <span>'+ 
-              formattedSize +
-              '</span></p><a class="file-uploader__remove" href="#">Cancel</a></div>'
-            );
-          }
-         
-        });
+          // Push a template to the domFileList array
+          domFileList.push('<div class="file-uploader__item"><p>'+ 
+            fileName +
+            ' <span>'+ 
+            formattedSize +
+            '</span></p><a class="file-uploader__remove" href="#">Cancel</a></div>'
+          );
+       }
 
-        // Redraw the file list
-        $(fileList).html(domFileList);
+      });
 
-      }
+      // Redraw the file list
+      thisFileList.html(domFileList);
 
     });
 
 
     $(document).on('click', fileRemoveAction, function(e) {
       e.preventDefault();
-      $(this).closest('.file-uploader__item').remove();
-      console.log(domFileList + allFiles);
+
+      var index = $(this).closest('.file-uploader__item').index();
+      var thisFileUploader = $(this).parents(fileUploader);
+
+      thisFileUploader.find('.file-uploader__action').get(index).remove();
+      $(this).parent('.file-uploader__item').remove();
+
+      
+
+      thisFileUploader.find('input:file').each( function() {
+
+        if (this.files.length) {
+          var fileName      = this.files[0].name;
+          var formattedSize = formatFileSizeString(this.files[0].size);
+
+          console.log(fileName);
+
+        }
+      });
+
     });
 
     // For IE users we bind a property change event
