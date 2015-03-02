861ec96d56fb9ceb95fc5963abbe80021396ad13
Sam Quayle <sam.quayle@valtech.co.uk>
2015-03-02 09:47:52 +0000

    Updates internal applications

--- assets/_js/common/fileUploader.js
+++ assets/_js/common/fileUploader.js
@@ -3,7 +3,7 @@ var OLCS = OLCS || {};
 /**
  * OLCS.fileUploader
  *
- * Enhances the file uploader 
+ * Enhances the file uploader
  *
  */
 
@@ -12,20 +12,21 @@ OLCS.fileUploader = (function(document, $, undefined) {
   'use strict';
 
   var fileUploader     = '.file-uploader';
-  var fileInput        = 'input[type=file]';
-  var fileUploadButton = '.action--primary, .action--secondary';
-  var fileList         = '.file-uploader__list';
-  var fileActions      = '.file-uploader__actions'
+  var fileActions      = '.file-uploader__actions';
   var fileAction       = '.file-uploader__action';
+  var fileUploadButton = '.action--primary, .action--secondary';
+  var fileInput        = 'input[type=file]';
   var fileRemoveAction = '.file-uploader__remove';
-  
+  var fileListItem     = '.file-uploader__item';
+  var fileList         = '.file-uploader__list';
+
   return function init() {
 
     function getFileName(path) {
-      var index = path.lastIndexOf("\\") + 1;
+      var index = path.lastIndexOf('\\') + 1;
       return path.substr(index);
     }
-  
+
     function formatFileSizeString(fileSizeInBytes) {
       var string = fileSizeInBytes.toString();
       var stringLength = string.length;
@@ -41,26 +42,23 @@ OLCS.fileUploader = (function(document, $, undefined) {
         return kilo.toFixed(0) + ' kB';
       }
       else if (stringLength <= 9) {
-        return mega.toFixed(1) + ' MB'; 
-      } 
+        return mega.toFixed(1) + ' MB';
+      }
       else {
-        return giga.toFixed(1) + ' GB'; 
+        return giga.toFixed(1) + ' GB';
       }
     }
 
-    function createNewFileAction(element,count) {
+    function createNewFileAction(current) {
 
-      // Clone the last file input
-      var newfileAction = element.last().clone();
+      var newfileAction = $(current).last().clone();
 
-      // Store a string version
-      var newHTML = newfileAction.html();
-      
-      // Find the ID
-      var regex = new RegExp(/fileUpload\[(.*?)\]/g);
+      var regex         = new RegExp(/fileUpload\[(.*?)\]/g);
+      var actionsCount  = $(current).find(fileAction).length + 1;
+      var newHTML       = newfileAction.html();
 
-      // Replace ID with the new incremented one 
-      newHTML = newHTML.replace(regex, "fileUpload["+count+"]");
+      // Replace ID with the new incremented one
+      newHTML = newHTML.replace(regex, 'fileUpload['+actionsCount+']');
 
       // Set the HTML of our new element
       newfileAction.html(newHTML);
@@ -70,8 +68,21 @@ OLCS.fileUploader = (function(document, $, undefined) {
 
     }
 
+    function removeFile(current) {
 
-    // When the file upload button is clicked trigger a 
+      var thisFileUploader = $(current).parents(fileUploader);
+      var thisListItem     = $(current).closest(fileListItem);
+      var thisIndex        = thisListItem.index();
+
+      // Remove the corresponding file input
+      thisFileUploader.find(fileAction).get(thisIndex).remove();
+
+      // Remove it's parent list item
+      thisListItem.remove();
+    }
+
+
+    // When the file upload button is clicked trigger a
     // click of the file input
     $(document).on('click', fileUploadButton, function(e) {
       e.preventDefault();
@@ -79,21 +90,20 @@ OLCS.fileUploader = (function(document, $, undefined) {
     });
 
     // When the file input is updated
-    $(document).on('change', fileInput, function() {    
-      ;
+    $(document).on('change', fileInput, function() {
+
       var thisFileUploader = $(this).parents(fileUploader);
       var thisFileAction   = thisFileUploader.find(fileAction);
       var thisFileList     = thisFileUploader.find(fileList);
       var domFileList      = [];
-      var noOfFileActions  = thisFileAction.length + 1;
 
       // Add the new file uploader
-      $(this).closest(fileActions).append(createNewFileAction(thisFileAction,noOfFileActions));
+      $(this).closest(fileActions).append(createNewFileAction(thisFileAction));
 
       // Hide the current file uploader
       $(this).closest(fileAction).hide();
-      
-      // Get every attached file 
+
+      // Get every attached file
       thisFileUploader.find(fileInput).each( function() {
 
         if (this.files.length) {
@@ -101,59 +111,53 @@ OLCS.fileUploader = (function(document, $, undefined) {
           var formattedSize = formatFileSizeString(this.files[0].size);
 
           // Push a template to the domFileList array
-          domFileList.push('<div class="file-uploader__item"><p>'+ 
+          domFileList.push('<div class="file-uploader__item"><p>'+
             fileName +
-            ' <span>'+ 
+            ' <span>'+
             formattedSize +
             '</span></p><a class="file-uploader__remove" href="#">Cancel</a></div>'
           );
-       }
+        }
 
       });
 
-      // Redraw the file list
       thisFileList.html(domFileList);
 
     });
 
+    //For IE users we bind a property change event
+    $(fileInput).on('propertychange', function() {
 
-    $(document).on('click', fileRemoveAction, function(e) {
-      e.preventDefault();
-
-      var index = $(this).closest('.file-uploader__item').index();
       var thisFileUploader = $(this).parents(fileUploader);
+      var thisFileAction   = thisFileUploader.find(fileAction);
+      var thisFileList     = thisFileUploader.find(fileList);
+      var domFileList      = [];
 
-      thisFileUploader.find('.file-uploader__action').get(index).remove();
-      $(this).parent('.file-uploader__item').remove();
+      // Add the new file uploader
+      $(this).closest(fileActions).append(createNewFileAction(thisFileAction));
 
-      
+      // Hide the current file uploader
+      $(this).closest(fileAction).hide();
 
-      thisFileUploader.find('input:file').each( function() {
+      thisFileUploader.find(fileInput).each( function() {
+        if (this.value) {
+          var fileName = getFileName(this.value);
 
-        if (this.files.length) {
-          var fileName      = this.files[0].name;
-          var formattedSize = formatFileSizeString(this.files[0].size);
-
-          console.log(fileName);
-
+          // Create a corresponding element and append it to the dom
+          thisFileList.html('<div class="file-uploader__item"><p>'+
+            fileName +
+            '</p><a class="file-uploader__remove" href="#">Cancel</a></div>'
+          );
         }
       });
 
+      thisFileList.html(domFileList);
     });
 
-    // For IE users we bind a property change event
-    $(fileInput).on('propertychange', function(e) {
-      
-      // If it's not empty
-      if (this.value) {
-        var fileName = getFileName(this.value);
-
-        // Create a corresponding element and append it to the dom
-        $('.file-uploader__list').html('<div class="file-uploader__item"><p>'+ 
-          fileName +
-          '</p><a class="file-uploader__remove" href="#">Cancel</a></div>'
-        );
-      }
+    // When a remove link is clicked remove the list item and file input
+    $(document).on('click', fileRemoveAction, function(e) {
+      e.preventDefault();
+      removeFile(this);
     });
 
   };
