aa156ce7d0dfe8370a851cd6a2bcb8efd406f6c4
Nick Payne <nick@kurai.co.uk>
2015-03-02 13:37:51 +0000

    fix response filter when filter is top-level element

--- assets/_js/common/responseFilter.js
+++ assets/_js/common/responseFilter.js
@@ -5,15 +5,32 @@ OLCS.responseFilter = (function(document, $, undefined) {
   "use strict";
 
   return function init(filter, container) {
+
+    if (!container) {
+      throw new Error("OLCS.responseFilter requires a container argument");
+    }
+
     return OLCS.normaliseResponse(function(response) {
       var content = response.body;
 
-      var filtered = $(content)
-        .find(filter)
-        .html();
+      if (filter) {
+        /**
+         * we MUST wrap the plain content in a container so that
+         * .find() works consistently; without this if the top-level
+         * element was the one we wanted to filter the find would fail
+         *
+         * Adding this container has no impact since we never actually
+         * mutate content with it; we're just temporarily adding it
+         * so we can search its children
+         */
+        var filtered = $("<div>")
+          .append(content)
+          .find(filter)
+          .html();
 
-      if (filtered) {
-        content = filtered;
+        if (filtered) {
+          content = filtered;
+        }
       }
 
       OLCS.formHelper.render(container, content);
