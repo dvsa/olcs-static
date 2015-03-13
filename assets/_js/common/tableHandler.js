ef1a55ddc6f4ee686ab92b6f3bb08dbe0cf4b6ee
Nick Payne <nick@kurai.co.uk>
2015-03-13 14:01:13 +0000

    rename and rescope table handler

--- assets/_js/common/tableHandler.js
+++ assets/_js/common/tableHandler.js
@@ -21,47 +21,29 @@ var OLCS = OLCS || {};
  * sense to split this into smaller ones which this just opts into
  */
 
-OLCS.tableHandler = (function(document, $, undefined) {
+OLCS.formHandlerModal = (function(document, $, undefined) {
 
   "use strict";
 
   return function init(options) {
-    var table = options.table;
-    var actionName = options.actionName || "action";
-    var actionSelector = options.selector || table + " .table__header [name=" + actionName + "], .table__empty button, " +
-      ".table__wrapper input[type=submit], .table__header [name='table[" + actionName + "]']";
+    var form = options.form;
     var bodySelector = options.bodySelector;
     var F = OLCS.formHelper;
 
-    /**
-     * Form action links
-     */
-    $(document).on("click", actionSelector, function(e) {
-      e.preventDefault();
-
-      var form   = $(this).parents("form");
-      var button = $(this);
-
-      // our backend logic relies on receiving the the value of the action
-      // button which was clicked in order to determine what to do.
-      // Unfortunately a JS click event + submit combo won't include
-      // that info, so we have to manually inject it into the form
-      F.pressButton(form, button);
-
-      // submit the *table* form...
-      OLCS.formAjax({
-        form: form,
-        success: OLCS.normaliseResponse(function(data) {  
-          if (data.hasErrors && bodySelector) {
-            F.render(bodySelector, data.body);
-            return;
-          }
-          // assume that the the modal we get back has a form,
-          // so invoke a wrapper component to bind a formHandler
-          // and show the modal at the same time
-          OLCS.formModal(data);
-        })
-      });
+    OLCS.formHandler({
+      form: form,
+      isModal: true,
+      onChange: false,
+      success: OLCS.normaliseResponse(function(data) {
+        if (data.hasErrors && bodySelector) {
+          F.render(bodySelector, data.body);
+          return;
+        }
+        // assume that the the modal we get back has a form,
+        // so invoke a wrapper component to bind a formHandler
+        // and show the modal at the same time
+        OLCS.formModal(data);
+      })
     });
   };
 
