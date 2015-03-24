1ab3e0b498a4971f80f6ab57b4f4857d494383b9
Rob Caiger <rob@clocal.co.uk>
2015-03-24 11:22:20 +0000

    Split screen js improvements

--- assets/_js/common/splitScreen.js
+++ assets/_js/common/splitScreen.js
@@ -12,73 +12,122 @@ OLCS.splitScreen = (function(document, $, undefined) {
 
   return function init() {
 
-    var isClosed = false;
-    var isPreloading;
+    var url1, url2, orientation, isVisible, mainFrame, sideFrame, panel, canStore;
 
-    var fragment = window.location.hash.substring(1);
-    var decoded = window.atob(fragment);
-    var parts = decoded.split('|');
+    /**
+     * Splits the hash fragment into the relevant variables, format is
+     *  #base64encode(url1|url2|orientation|isVisible)
+     */
+    function splitHashFragment() {
+      var parts = window.atob(window.location.hash.substring(1)).split('|');
 
-    var url1 = parts[0];
-    var url2 = parts[1];
-    var orientation = parts[2];
-
-    var mainFrame = $('#iframe-one');
-    var sideFrame = $('#iframe-two');
-    var panel = $('.iframe-controls');
-
-    var canStore;
-
-    try {
-      localStorage.setItem('checkStore', true);
-      canStore = true;
-    } catch (err) {
-      canStore = false;
+      url1 = parts[0];
+      url2 = parts[1];
+      orientation = parts[2];
+      isVisible = parts[3] === '0' ? false : true;
     }
 
+    /**
+     * Finds the panel and the iframe dom elements
+     */
+    function findComponents() {
+      mainFrame = $('#iframe-one');
+      sideFrame = $('#iframe-two');
+      panel = $('.iframe-controls');
+    }
+
+    /**
+     * Checks whether we can access local storage for preferences
+     */
+    function checkStore() {
+      try {
+        localStorage.setItem('checkStore', true);
+        canStore = true;
+      } catch (err) {
+        canStore = false;
+      }
+    }
+
+    function isValidOrientation(orientation) {
+      return $.inArray(orientation, ['vertical', 'horizontal', 'closed']) !== -1;
+    }
+
+    /**
+     * Grabs the default orientation,
+     *  - checks the hash fragment first
+     *  - then checks local storage preferences
+     */
     function getDefaultOrientation() {
-      // If we have a valid orientation in the hash, use that
-      if ($.inArray(orientation, ['vertical', 'horizontal']) !== -1) {
+
+      if (isValidOrientation(orientation)) {
         return orientation;
       }
 
-      // See if we can check the localStorage
       var defaultOrientation = getOrientationPreference();
 
-      // If we still don't have a valid orientation, set to default
-      if ($.inArray(defaultOrientation, ['vertical', 'horizontal']) === -1) {
+      if (!isValidOrientation(defaultOrientation)) {
         defaultOrientation = 'horizontal';
       }
 
       return defaultOrientation;
     }
 
+    /**
+     * Grab the orientation preference from local storage if we can
+     */
     function getOrientationPreference() {
       if (canStore) {
         return localStorage.getItem('OLCS.preferences.splitscreen.orientation');
       }
     }
 
+    /**
+     * Save our current orientation to the preferences local storage if we can
+     *
+     * @param {string} orientation
+     */
     function setOrientationPreference(orientation) {
       if (canStore) {
         localStorage.setItem('OLCS.preferences.splitscreen.orientation', orientation);
       }
     }
 
+    /**
+     * Called when the close button is clicked
+     */
     function close() {
+      // We need to update the hash to remember that we are closed
+      //  in case someone refreshes the page
       orientation = 'closed';
-      updateHash();
+      updateHashFragment();
+
+      // Resize the iframe
       mainFrame.attr('class', 'iframe--full');
+
+      // We no longer care about the side frame or panel so we can remove them
       sideFrame.remove();
       panel.remove();
-      isClosed = true;
+
+      mainFrame.contents().find('form,a').each(function () {
+        if (!$(this).attr('target')) {
+          $(this).attr('target', '_parent');
+        }
+      });
     }
 
-    function updateHash() {
-      var string = url1 + '|' + url2 + '|' + orientation;
+    /**
+     * Update the has fragment in the url
+     */
+    function updateHashFragment() {
+      var string = url1 + '|' + url2 + '|' + orientation + '|' + (isVisible ? '1' : '0');
       window.location.hash = '#' + window.btoa(string);
     }
 
+    /**
+     * Set the orientation of the split screen
+     *
+     * @param {string} newOrientation
+     */
     function setOrientation(newOrientation) {
       orientation = newOrientation;
 
@@ -88,58 +137,58 @@ OLCS.splitScreen = (function(document, $, undefined) {
       sideFrame.attr('class', 'iframe--' + orientation);
       panel.attr('class', 'iframe-controls ' + orientation);
 
-      updateHash();
+      updateHashFragment();
     }
 
-    if (orientation === 'closed') {
-      close();
-    } else {
-
-      isPreloading = true;
-      OLCS.preloader.show();
-
-      setOrientation(getDefaultOrientation());
+    /**
+     * Called when we toggle collapsing the split screen
+     */
+    function toggleVisible() {
+      isVisible = !isVisible;
+      updateHashFragment();
+      panel.toggleClass('collapsed');
+      $('#iframe-two').toggleClass('collapsed');
+      $('#iframe-one.iframe--horizontal').toggleClass('full');
     }
 
+    splitHashFragment();
+
+    findComponents();
+
+    // Start loading the iframes
     mainFrame.attr('src', url1);
     sideFrame.attr('src', url2);
 
-    // Need a better solution for this
-    mainFrame.load(function() {
+    checkStore();
 
-      url1 = window.frames['iframe-one'].location.href;
+    if (orientation === 'closed') {
+      // In this case, we have to wait for the mainFrame to load before calling close, otherwise it won't
+      //  set target parent on anything
+      mainFrame.load(function() {
+        close();
+      });
+    } else {
+      setOrientation(getDefaultOrientation());
+    }
 
-      updateHash();
+    if (!isVisible) {
+      toggleVisible();
+    }
 
-      if (isClosed) {
-        $('body').html(mainFrame.contents().find('body').html());
-
-        $('body').find('form').each(function () {
-          if (!$(this).attr('action') || $(this).attr('action') === '') {
-            $(this).attr('action', url1);
-          }
-        });
-      }
-
-      // Need to only hide this the first time
-      if (isPreloading) {
-        OLCS.preloader.hide();
-        isPreloading = false;
-      }
-    });
-
-    $(document).on('click', '.iframe-controls--orientation', function toggleOrientation() {
+    // Listeners
+    $(document).on('click', '.iframe-controls--orientation', function toggledOrientation(e) {
+      e.preventDefault();
       setOrientation(orientation === 'vertical' ? 'horizontal' : 'vertical');
     });
 
-    $(document).on('click', '.iframe-controls--toggle', function(e) {
+    $(document).on('click', '.iframe-controls--toggle', function toggledVisibility(e) {
       e.preventDefault();
-      $(this).parent('.iframe-controls').toggleClass('collapsed');
-      $('#iframe-two').toggleClass('collapsed');
-      $('#iframe-one.iframe--horizontal').toggleClass('full');
+      toggleVisible();
     });
 
-    $(document).on('click', '.iframe-controls--close', close);
+    $(document).on('click', '.iframe-controls--close', function closeSplitScreen(e) {
+      e.preventDefault();
+      close();
+    });
   };
-
 }(document, window.jQuery));
