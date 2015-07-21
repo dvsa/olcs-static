33094adcc92fe8a09fd9244f27c3ce1afef47999
Sam Quayle <sam.quayle@valtech.co.uk>
2015-07-21 11:41:52 +0100

    Adds authentication prototype screens to styleguide

--- assets/_js/init/selfserve.js
+++ assets/_js/init/selfserve.js
@@ -1,21 +1,9 @@
 OLCS.ready(function() {
   "use strict";
 
-  // Quick and dirty way of setting the logged in users
-  // details in localStorage
-
-	var user = {
-    name: localStorage.name,
-    company: localStorage.company
-	};
-
-  if (user.name && user.company) {
-    $(".user-menu__item:first").html(""+user.name+", <b>"+user.company+"</b>");
-
-    if ($(".page-header__subtitle").text() === "Operator Licensing Ltd.") {
-      $(".page-header__subtitle").text(user.company);
-    }
-  }
-
+  OLCS.toggleElement({
+    triggerSelector: ".proposition__toggle",
+    targetSelector: ".proposition-nav"
+  });
 
 });
