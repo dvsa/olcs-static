835ae6d2fef4dd04e82afb699a47b1c2ba85fab7
Edmund Reed <esr360@live.com>
2016-01-06 10:29:13 +0000

    removing $ from var names

--- assets/_js/common/formSubmit.js
+++ assets/_js/common/formSubmit.js
@@ -15,20 +15,20 @@ OLCS.formSubmit = (function(document, $, undefined) {
   return function init(options) {
     
     // Get the selectors of interest
-    var $form = options.form;
-    var $submit = options.submit;
+    var form = options.form;
+    var submit = options.submit;
     
     // Create a variable to store the submit button text
     var $submitText;
         
     // Iterate through each occurance
-    $($form).each(function() {
+    $(form).each(function() {
       
       // Cache each individual form
-      var $thisForm = $(this);
+      var thisForm = $(this);
       
       // When a submit button is clicked
-      $($submit).unbind().on('click', function() {
+      $(submit).unbind().on('click', function() {
     
         // Set appropriate button replacement message
         // If clicked element has attirbute 'data-onclick-become',
@@ -43,7 +43,7 @@ OLCS.formSubmit = (function(document, $, undefined) {
         $submitText = $(this).html();
         
         // Disable all the submit buttons in the current form
-        $thisForm.find($submit).addClass('disabled');
+        thisForm.find(submit).addClass('disabled');
         
         // Replace the clicked button text with an appropriate message
         // If no message is set, the button text will remain as normal
@@ -61,7 +61,7 @@ OLCS.formSubmit = (function(document, $, undefined) {
       function revertFormSubmit() {
         
         // Re-enable all the submit buttons in the current form
-        $($submit).removeClass('disabled');
+        $(submit).removeClass('disabled');
         
         // Replace the loading text with the original text
         $('.submit-clicked').html($submitText).removeClass('submit-clicked');
