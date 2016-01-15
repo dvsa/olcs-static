067149b17497f6900e35137839f50bcbb8aeb8cb
Edmund Reed <esr360@live.com>
2016-01-15 14:28:22 +0000

    changing code to run on render as opposed to click

--- assets/_js/common/textareaPlaceholder.js
+++ assets/_js/common/textareaPlaceholder.js
@@ -15,15 +15,16 @@ OLCS.textareaPlaceholder = (function(document, $, undefined) {
   'use strict';
 
   return function init(options) {
-  
-    // When any textarea HTML element is clicked
-    $(document).on("click", "textarea", function() {
-      // Check to see if the placeholder value equals the actual value
-      if ($(this).attr('placeholder') == $(this).val()) {
-        // If so, empty the actual value
-        $(this).val("");
-      }
-    });  
+    
+    OLCS.eventEmitter.on("render", function() {
+      $('textarea').each(function() {
+        // Check to see if the placeholder value equals the actual value
+        if ($(this).attr('placeholder') == $(this).val()) {
+          // If so, empty the actual value
+          $(this).val("");
+        }
+      });
+    });
 
   };
 
