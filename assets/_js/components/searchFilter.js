8ce715ea72f245a299384eb46fe1863609cf4d52
Edmund Reed <esr360@live.com>
2016-03-24 12:18:43 +0000

    making new searchFilter js component accessible

--- assets/_js/components/searchFilter.js
+++ assets/_js/components/searchFilter.js
@@ -15,32 +15,64 @@ var OLCS = OLCS || {};
 
     var options = $.extend({
       parent  : '.search-filter',
-      title   : 'h3',
       content : '.form__filter',
+      title   : 'h3',
       class   : 'toggled',
-      mobile  : 780
+      mobile  : '780px'
     }, custom);
+      
+    var title   = $(options.parent).find(options.title);
+    var content = $(options.parent).find(options.content);
+    
+    // Get the ID's of the elements of interest
+    var titleID   = title.attr('id');
+    var contentID = content.attr('id');
     
     function makeExpandable() {
-      $(options.parent).find(options.content).hide();
-      $(options.title).click(function() {
-        $(options.content).toggle();
-        $(options.content).parents(options.parent).toggleClass(options.class);
+      
+      $(options.parent).removeClass(options.class);
+      
+      title.attr({
+        'aria-expanded' : 'false',
+        'aria-controls' : contentID
       });
-    }
+      
+      content.hide().attr({
+        'aria-hidden' : 'true',
+        'aria-labelledby' : titleID
+      });
+      
+      title.click(function() {
+        
+        console.log('test');
+        
+        content.toggle();
+        
+        if (content.is(':visible')) {
+          title.attr('aria-expanded', 'true');
+          content.attr('aria-hidden', 'false');
+        } else {
+          title.attr('aria-expanded', 'false');
+          content.attr('aria-hidden', 'true');
+        }
+        
+        $(options.parent).toggleClass(options.class);
+        
+      });
+      
+    } // makeExpandable()
     
     function revert() {
-      $(options.parent).find(options.content).show();
-      $(options.content).parents(options.parent).removeClass(options.class);
-      $(options.title).click(function() {
-        return;
-      });
+      title.removeAttr('aria-expanded aria-controls');
+      content.show().removeAttr('aria-hidden aria-labelledby');
+      $(options.parent).removeClass(options.class);
+      title.click(function() { return });
     }
 
-    // Run the code on page load & resize
-    $(window).on('load resize', function() {
-      if (($(document).width()) < options.mobile) {
-        makeExpandable();
+    // Run the code on page load
+    $(window).on('load', function() { 
+      if (window.matchMedia('(max-width: ' + options.mobile + ')').matches) {
+        makeExpandable(); 
       } else {
         revert();
       }
