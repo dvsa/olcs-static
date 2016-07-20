3e72dbd67116a2ce00b869ddf5a0f24cae782922
Edmund Reed <esr360@live.com>
2016-07-20 13:04:57 +0100

    improving test coverage

--- test/js/modal.test.js
+++ test/js/modal.test.js
@@ -24,13 +24,16 @@ describe('OLCS.modal', function() {
   });
 
   describe('show', function() {
-
     beforeEach(function() {
       this.component.show('body here', 'title here');
     });
 
     afterEach(function() {
       this.component.hide();
+    });
+
+    it('shows the component', function() {
+      expect(this.component.isVisible()).to.be(true);
     });
 
     it('shows the modal overlay', function() {
@@ -49,36 +52,62 @@ describe('OLCS.modal', function() {
       expect($('.modal__content').text()).to.equal('body here');
     });
 
-    it('shows the modal overlay', function() {
+    it('should have a close button', function() {
+      expect($('.modal__close').length).to.be(1);
+    });
+
+    describe('When clicking the modal close button', function() {
+      beforeEach(function() {
+        this.spy = sinon.stub(this.component, 'hide');
+        $('.modal__close').trigger('click');
+      });
+
+      afterEach(function() {
+        this.spy.restore();
+      });
+
+      it('invokes the modal’s hide method', function() {
+        expect(this.spy.called).to.be(true);
+      });
+    });
+
+    describe('when pressing the Esc button', function() {
+      beforeEach(function() {
+        var e = jQuery.Event('keyup');
+        e.keyCode = 27;
+        $(document).trigger(e);
+      });
+
+      it('hides the modal', function() {
+        expect(this.component.isVisible()).to.be(false);
+      });
+    });
+  }); // show
+
+  describe('Show with a "close-trigger" data-attribute', function() {
+    beforeEach(function() {
+      $(document).on('click', '.action', function() {
+        $('body').append('<div id="foo"></div>');
+      });
+      var $content = [
+        '<form data-close-trigger=".action">',
+          '<div class="action"></div>',
+        '</form>',
+      ].join('\n');
+      this.component.show($content);
+      this.component.hide();
+    });
+
+    it('should not close the component', function() {
       expect(this.component.isVisible()).to.be(true);
     });
     
-    describe('When chosenJS dropdown is present', function() {
-    
-      beforeEach(function() {
-        OLCS.eventEmitter.emit('render');
-        $('.modal__wrapper').append([
-          '<select id="stub-select">',
-            '<option value="foo">Foo</option>',
-            '<option value="bar">bar</option>',
-            '<option value="baz">baz</option>',
-            '<option value="quz">quz</option>',
-          '</select>',
-        ].join('\n'));
-        $('#stub-select').chosen({ width: '350px' });
-        OLCS.eventEmitter.emit('render');
-      });
-      
-      it('should convert the select dropdown in to a Chosen dropdown', function() {
-         expect($('.chosen-container').length).to.equal(1);
-      });
-
-    }); // When chosenJS dropdown is present
-
-  }); // show
+    it('should trigger a click of the ".action" element', function() {
+      expect($('#foo').length).to.be(1);
+    });
+  }); // Show with a "close-trigger" data-attribute
 
   describe('show without a passed title', function() {
-
     beforeEach(function() {
       this.component.show('body here');
     });
@@ -102,11 +131,9 @@ describe('OLCS.modal', function() {
     it('applies the correct body', function() {
       expect($('.modal__content').text()).to.equal('body here');
     });
-
   }); // show without passed title
 
   describe('hide', function() {
-
     beforeEach(function() {
       this.eventSpy = sinon.spy(OLCS.eventEmitter, 'emit');
       this.component.hide();
@@ -127,80 +154,9 @@ describe('OLCS.modal', function() {
     it('emits the correct event', function() {
       expect(this.eventSpy.firstCall.args[0]).to.equal('hide:modal');
     });
-
   }); // hide
 
-  describe('Given a stubbed DOM with a modal close button', function() {
-
-    beforeEach(function() {
-      $('body').append([
-        '<div class=modal__wrapper>',
-          '<a href=# class=modal__close>Close</a>',
-        '</div>'
-      ].join('\n'));
-    });
-
-    afterEach(function() {
-      $('.modal__wrapper').remove();
-    });
-
-    it('should have a close button', function() {
-      expect($('.modal__close').length).to.be(1);
-    });
-
-    it('should have a modal wrapper', function() {
-      expect($('.modal__wrapper').length).to.be(1);
-    });
-
-    describe('When clicking the modal close button', function() {
-
-      beforeEach(function() {
-        this.spy = sinon.stub(this.component, 'hide');
-        $('.modal__close').trigger('click');
-      });
-
-      afterEach(function() {
-        this.spy.restore();
-      });
-
-      it('invokes the modal’s hide method', function() {
-        expect(this.spy.called).to.be(true);
-      });
-
-    });
-    
-    describe('When a form with a close-trigger data attribute is present', function() {
-
-      beforeEach(function() {
-        $('.modal__content').append([
-          '<form data-close-trigger="">',
-          '</form>'
-        ].join('\n'));
-        this.component.hide();
-      });
-      
-      it('should correctly hide the modal', function() { 
-        expect(this.component.isVisible()).to.be(false);
-      });
-
-    }); // When a form with a close-trigger data attribute is present
-
-    describe('when pressing the Esc button', function() {
-
-      beforeEach(function() {
-        $(document).triggerHandler({type: 'keyup', which: 27});
-      });
-
-      it.skip('hides the modal', function() {
-        expect(this.component.isVisible()).to.be(false);
-      });
-
-    });
-
-  }); // Given a stubbed DOM with a modal close button
-
   describe('Simulate mobile experience', function() {
-    
     beforeEach(function() {
       $('body').append([
         '<div class=modal__wrapper>',
@@ -226,11 +182,9 @@ describe('OLCS.modal', function() {
         expect($('.modal__wrapper').css('position')).to.equal('static');
       });
     });
-
   }); // Simulate mobile experience
 
   describe('When invoked when a modal is already open', function() {
-    
     beforeEach(function() {
       $('body').append([
         '<div class="overlay" style="display:none;"></div>'
@@ -245,7 +199,6 @@ describe('OLCS.modal', function() {
     it('hides the old modal and shows the new one', function() {
       expect($('.overlay').is(':visible')).to.be(true);
     });
-
   }); // When invoked when a modal is already open
 
 });
