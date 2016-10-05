2df4a18a1237f28c05e944c498b36f85b6306d93
Edmund Reed <esr360@live.com>
2016-10-05 16:18:33 +0100

    mvp for new disable submit button on form submit

--- assets/_js/components/disableForm.js
+++ assets/_js/components/disableForm.js
@@ -16,8 +16,37 @@ OLCS.disableForm = (function(document, $, undefined) {
   return function init(custom) {
 
     var options = $.extend({
-      errorContainer : '',
+      actions: '[type="submit"], [class*="action-"]',
+      disabledClass: 'disabled',
+      loadingText: 'Loading...'
     }, custom);
+
+    // Execute code on all potential actions
+    $(options.actions).each(function() {
+
+      // Cache trigger selector
+      var trigger = $(this);
+
+      trigger.on('click', function(e) {
+
+        var target = $(this);
+        var container = target.parents('.actions-container') || target.parents('form');
+        var formActions = container.find(options.actions);
+
+        // Add disabled class to relevant actions
+        formActions.addClass(options.disabledClass);
+
+        // Change target button text during interim
+        if (options.loadingText) {
+          // Ensure button's width does not change/jump
+          target.css('width', target.outerWidth());
+          // Replace button's text with loading text
+          target.text(options.loadingText);
+        }
+
+      });
+
+    });
 
   };
 
