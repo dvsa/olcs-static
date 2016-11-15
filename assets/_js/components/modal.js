3e28a80daba58486b230d7c25a1ff34737c90f28
Edmund Reed <esr360@live.com>
2016-11-15 13:12:02 +0000

    fixing broken unit tests due to undefined tinymce

--- assets/_js/components/modal.js
+++ assets/_js/components/modal.js
@@ -127,7 +127,10 @@ OLCS.modal = (function(document, $, undefined) {
     $(wrapper +','+overlay).remove();
 
     // Unbind any Tinymce
-    tinymce.remove(); // jshint ignore:line
+
+    if (typeof(tinymce) !== 'undefined') {
+      tinymce.remove(); // jshint ignore:line
+    }
 
     // Set the aria-hidden attribute of all other content to 'false'
     // when the modal closes
