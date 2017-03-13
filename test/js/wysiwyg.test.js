8e30b268a2e3ef00ff18877aee2afe137a43c22e
Edmund Reed <esr360@live.com>
2017-03-13 12:17:46 +0000

    updating tinymce unit tests

--- test/js/wysiwyg.test.js
+++ test/js/wysiwyg.test.js
@@ -11,28 +11,54 @@ describe('OLCS.wysiwyg', function() {
     this.component = OLCS.wysiwyg;
   });
 
-  beforeEach(function() {
-    $('body').append([
-      '<textarea id="stub" class="tinymce"></textarea>'
-    ].join('\n'));
-  });
-
   it('should be defined', function() {
     expect(this.component).to.exist;
   });
 
-  it('should successfullly create a TinyMCE instance', function() {
+  it('should expose the TinyMCE method', function() {
     expect(typeof(tinymce)).to.not.be('undefined');
   });
 
   describe('when initialised with default options', function() {
 
     beforeEach(function() {
+      $('body').append([
+        '<textarea id="stub" class="tinymce"></textarea>'
+      ].join('\n'));
       this.component();
+      OLCS.eventEmitter.emit('render');
     });
 
     it('should successfullly create a TinyMCE instance', function() {
-      expect(tinymce.EditorManager.editors).to.eql([]);
+      expect($('.mce-tinymce').length).to.equal(1);
+      expect($('.mce-tinymce iframe').length).to.equal(1);
+      expect(tinymce.EditorManager.editors.length).to.equal(1);
+    });
+
+    afterEach(function() {
+      $('#stub').remove();
+    });
+
+  });
+
+  describe('when initialised with ajaxified content within a modal', function() {
+
+    beforeEach(function() {
+      $('body').append('<a id="stub" class="js-modal-ajax" href="test.html">Click me</a>');
+
+      this.component();
+      
+      $('#stub').click();
+
+      OLCS.eventEmitter.emit('render');
+    });
+
+    it('should make an ajax request', function() {
+      expect(sinon.stub(OLCS, 'ajax').calledOnce).to.equal(true);
+    });
+
+    afterEach(function() {
+      $('#stub').remove();
     });
 
   });
