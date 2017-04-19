4cd329f3305fc1bb798ee178c00362fe6240bb37
jamie crisp <james.crisp@valtech.co.uk>
2017-04-19 16:38:31 +0100

    OCLS-14599: added unit tests

--- assets/_js/internal/submissionRecommendation.js
+++ assets/_js/internal/submissionRecommendation.js
@@ -1,30 +1,6 @@
-/**
- * 1) Defensively declare our project-level namespace
- */
 var OLCS = OLCS || {};
 
-/**
- * 2) Attach our component to our namespace using an
- * IIFE (Immediately Invoked Function Expression).
- *
- * Doing so means that everything within the function
- * has its own scope; this leaves it up to us what we
- * expose as the component's public interface.
- *
- * Use camelCase to name your component, and unless
- * you have a good reason not to, leave the arguments
- * as follows:
- *
- * document - this injects the global document variable locally,
- * meaning we avoid a scope lookup when accessing it within
- * the component
- *
- * $ - the jQuery variable, again scoped locally
- *
- * undefined - an edge case, but this prevents accidental reassignment
- * of what undefined represents and means you can test against it
- * directly
- */
+
 OLCS.submissionRecommendation = (function(document, $, undefined) {
   "use strict";
 
@@ -32,19 +8,27 @@ OLCS.submissionRecommendation = (function(document, $, undefined) {
 
   exports.addChangeEvent = function(options) {
     $(options.source).on("change", function() {
-      exports.changeEvent.call(this,options);
+      exports.removeRevokations(options);
     });
   };
 
+  // exports.setOriginalHTML = function(html){
+  //   olcs.submissionRecommendation.originalHTML = html;
+  // };
 
-  exports.changeEvent = function(options){
+  // export.getOriginalHTML = function(){
+  //   return this.originalHTML;
+  // };
+
+  exports.removeRevokations = function(options){
+
 
     var original = $(options.dest).clone();
     var subset   = original.clone();
     subset.find("[data-in-office-revokation=N]").remove();
 
     var contents;
-    var recommendations = $(this).val();
+    var recommendations = $(options.source)[0].value;
 
     if (recommendations && recommendations.indexOf(options.target) >= 0) {
       contents = subset.html();
@@ -54,7 +38,6 @@ OLCS.submissionRecommendation = (function(document, $, undefined) {
 
     $(options.dest).html(contents).trigger("chosen:updated");
   };
-
 
   return exports;
 
