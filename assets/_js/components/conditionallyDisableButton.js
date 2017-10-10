0b31c1b41f4b8731daf54edc37591b57e6e98da1
BJSS\Ammar.Haider <ammar.haider@bjss.com>
2017-10-10 14:34:04 +0100

    adds support for conditionally disbaling two buttons. Provides a warning in the console if the values provided in the options object don't match what was found in DOM

--- assets/_js/components/conditionallyDisableButton.js
+++ assets/_js/components/conditionallyDisableButton.js
@@ -8,8 +8,10 @@ var OLCS = OLCS || {};
  * wrapping <tr>. Example options:
  * {
  *  dataElSelector: '[data-heading="Document status"]',
- *  dataElValToCheck: 'New',
- *  buttonSelector: '#publish',
+ *  dataElValToCheck1: 'New',
+ *  dataElValToCheck2: 'Generated',
+ *  buttonSelector1: '#publish',
+ *  buttonSelector2: '#generate',
  *  stateAttr: 'disabled',
  *  stateAttrVal: true
  * }
@@ -24,20 +26,23 @@ OLCS.conditionallyDisableButton = (function (document, $, undefined) {
 
   return function init(options) {
 
-    if (!options || !options.dataElSelector || !options.dataElValToCheck || !options.buttonSelector || !options.stateAttr || !options.stateAttrVal) {
-      throw new Error('OLCS.disableButtonStateOnChange requires a dataElSelector, dataElValToCheck, buttonSelector, stateAttrVal and a stateAttr option');
+    if (!options || !options.dataElSelector || !options.dataElValToCheck1 || !options.dataElValToCheck2 || !options.buttonSelector1 || !options.buttonSelector2 || !options.stateAttr || !options.stateAttrVal) {
+      throw new Error('OLCS.disableButtonStateOnChange requires a dataElSelector, dataElValToCheck1, dataElValToCheck2, buttonSelector1, buttonSelector2, stateAttrVal and a stateAttr option');
     }
 
     exports.onChange = function() {
       var $checkedInput = $('input:checked');
       if ($checkedInput.length > 0) {
         var dataElVal = $checkedInput.parents('tr').find(options.dataElSelector).text();
-        var disableButton = dataElVal === options.dataElValToCheck;
-        if (disableButton) {
-          return $(options.buttonSelector).attr(options.stateAttr, options.stateAttrVal);
-        } else {
-          return $(options.buttonSelector).removeAttr(options.stateAttr);
+        var disableButton1 = dataElVal === options.dataElValToCheck1;
+        var disableButton2 = dataElVal === options.dataElValToCheck2;
+        if (disableButton1) {
+          return $(options.buttonSelector1).attr(options.stateAttr, options.stateAttrVal);
         }
+        if (disableButton2) {
+          return $(options.buttonSelector2).attr(options.stateAttr, options.stateAttrVal);
+        }
+        return OLCS.logger.warn('Data options supplied to OLCS.conditionallyDisableButton (dataElValToCheck1/dataElValToCheck2) did not match value found in DOM element: ' + options.dataElSelector);
       }
     };
 
