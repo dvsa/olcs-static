bd6e9a2b257646a108ed5e2fd3a23ea6203db8ab
Chris Emerson <Chris.Emerson@bjss.com>
2017-11-29 14:20:31 +0000

    Add Multiple functionality added to olcs-static. It duplicates the addAnother functionality, but for whole fieldsets & in the way Zend recommend you do it, via a template

---
+++ assets/_js/components/addMultiple.js
@@ -0,0 +1,38 @@
+var OLCS = OLCS || {};
+
+/**
+ * Add multiple
+ *
+ * Uses the Zend method for form collections, using the template generated instead of
+ * just duplicating the fieldset as 'addAnother' does
+ */
+OLCS.addMultiple = (function (document, $) {
+    'use strict';
+
+    return function init(custom) {
+        var options = $.extend({
+            container: '.add-multiple',
+            triggerSelector: '.hint input[type="submit"]',
+            targetSelector: 'span[data-template]'
+        }, custom);
+
+        OLCS.eventEmitter.once('render', function () {
+            // Run the plugin on each container
+            $(options.container).each(function () {
+                var container = $(this);
+                var triggerSelector = container.find(options.triggerSelector);
+
+                triggerSelector.on('click', function (e) {
+                    var template = container.find(options.targetSelector).data('template');
+
+                    var currentCount = container.find('> fieldset').length;
+                    template = template.replace(/__index__/g, currentCount);
+
+                    container.find(options.targetSelector).before(template);
+
+                    e.preventDefault();
+                });
+            });
+        });
+    };
+}(document, window.jQuery));
